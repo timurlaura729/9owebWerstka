6c3509a039adc8bb96a14c30ddbee186ad8a78d2
Dj_sunrise <Dj_sunrise@mail.ru>
2020-03-06 16:25:35 +0600

    Карта в футере

--- markup/static/js/separate-js/scripts.js
+++ markup/static/js/separate-js/scripts.js
@@ -2,6 +2,46 @@
 
 $(function() {
 
+    (function($) {
+
+        $.fn.bmdIframe = function( options ) {
+            var self = this;
+            var settings = $.extend({
+                classBtn: '.bmd-modalButton',
+                defaultW: 640,
+                defaultH: 360
+            }, options );
+
+            $(settings.classBtn).on('click', function(e) {
+                var allowFullscreen = $(this).attr('data-bmdVideoFullscreen') || false;
+
+                var dataVideo = {
+                    'src': $(this).attr('data-bmdSrc'),
+                    'height': $(this).attr('data-bmdHeight') || settings.defaultH,
+                    'width': $(this).attr('data-bmdWidth') || settings.defaultW
+                };
+
+                if ( allowFullscreen ) dataVideo.allowfullscreen = "";
+
+                // stampiamo i nostri dati nell'iframe
+                $(self).find(".js-iframe-video").attr(dataVideo);
+            });
+
+            // se si chiude la modale resettiamo i dati dell'iframe per impedire ad un video di continuare a riprodursi anche quando la modale è chiusa
+            this.on('hidden.bs.modal', function(){
+                $(this).find('.js-iframe-video').html("").attr("src", "");
+            });
+
+            return this;
+        };
+
+    })(jQuery);
+
+
+    jQuery(document).ready(function(){
+        jQuery("#iframe-modal").bmdIframe();
+    });
+
     /*
     |--------------------------------------------------------------------------
     | Sticky Kit
